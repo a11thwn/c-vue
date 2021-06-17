dedf391ef172fb8ca4f8c8380be844a69007cdc6
a11thwn <althwncc@gmail.com>
2021-06-17 19:41:02 +0800

    v-if

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,7 @@
 <template>
   <h3>{{ name }}</h3>
+  <div v-if="visible">隐藏的内容！</div>
+  <button @click="visible = !visible">{{ visible ? '隐藏' : '显示' }}</button>
 </template>
 
 <script>
@@ -7,6 +9,7 @@ export default {
   data() {
     return {
       name: 'NINGHAO',
+      visible: false,
     };
   },
 };
