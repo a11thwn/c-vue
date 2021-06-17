67a8648ef000e883a7e328951da874cc26a9a692
a11thwn <althwncc@gmail.com>
2021-06-17 19:25:58 +0800

    学习基础组件模板

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,8 +1,5 @@
 <template>
   <h3>{{ name }}</h3>
-  {{ processStatus }}
-  <button v-on:click="resetName">重置</button>
-  <button @click="setName">设置</button>
 </template>
 
 <script>
@@ -12,41 +9,7 @@ export default {
       name: 'NINGHAO',
     };
   },
-
-  computed: {
-    processStatus() {
-      return this.name === 'NINGHAO' ? '初始化...' : '成功设置了数据！';
-    },
-  },
-
-  watch: {
-    name(newName, oldName) {
-      console.log(`name 发生了变化：${oldName} -> ${newName}`);
-    },
-  },
-
-  created() {
-    console.log('App 组件已创建！');
-
-    this.setName();
-  },
-
-  methods: {
-    setName() {
-      setTimeout(() => {
-        this.name = '宁皓网';
-      }, 3000);
-    },
-
-    resetName() {
-      this.name = 'NINGHAO';
-    },
-  },
 };
 </script>
 
-<style>
-button {
-  margin: 4px;
-}
-</style>
+<style></style>
