8256484c1b08c3559fbeb8888ed483ba22a90e04
a11thwn <althwncc@gmail.com>
2021-06-17 19:56:18 +0800

    class

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,15 +1,39 @@
 <template>
   <h3>{{ name }}</h3>
+  <div class="menu">
+    <div
+      :class="['menu-item', { active: currentItem === index }]"
+      @click="
+        {
+          currentItem = index;
+        }
+      "
+      v-for="(item, index) in menuItems"
+      :key="index"
+    >
+      {{ item }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      name: 'NINGHAO',
+      name: '宁皓网',
+      menuItems: ['首页', '热门', '发布'],
+      currentItem: 0,
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+.menu {
+  display: flex;
+  gap: 16px;
+}
+.active {
+  color: #64359c;
+}
+</style>
