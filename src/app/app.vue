caa6754a6b89dc445cc261515b68b6eaa89658d1
a11thwn <althwncc@gmail.com>
2021-06-17 19:37:04 +0800

    v-for

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,8 @@
 <template>
   <h3>{{ name }}</h3>
+  <div v-for="(post, index) in postList" :key="post.id">
+    {{ index + 1 }} {{ post.content }} - <small>{{ post.author }}</small>
+  </div>
 </template>
 
 <script>
@@ -7,6 +10,24 @@ export default {
   data() {
     return {
       name: 'NINGHAO',
+
+      postList: [
+        {
+          id: 1,
+          content: '故人西辞黄鹤楼，烟花三月下扬州',
+          author: '李白',
+        },
+        {
+          id: 2,
+          content: '故人西辞黄鹤楼，烟花三月下扬州2',
+          author: '李白2',
+        },
+        {
+          id: 3,
+          content: '故人西辞黄鹤楼，烟花三月下扬州3',
+          author: '李白3',
+        },
+      ],
     };
   },
 };
